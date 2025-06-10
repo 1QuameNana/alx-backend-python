5449886b2141dda68fbac28501f56b8ea7fd449b
Kyei Cundy <nabcoasfeg14756@gmail.com>
2025-06-10 13:52:08 +0100

    views added

--- messaging_app/messaging_app/chats/serializers.py
+++ messaging_app/messaging_app/chats/serializers.py
@@ -19,3 +19,64 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['id', 'participants', 'messages', 'created_at']
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the custom User model.
+    Includes basic profile fields.
+    """
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'avatar']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Message model.
+    Includes nested sender (User) details.
+    """
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'content', 'timestamp']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Conversation model.
+    Includes nested participants and messages.
+    """
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'messages', 'created_at']
+
+
+class ConversationCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for creating a conversation.
+    Accepts a list of participant user IDs.
+    """
+    participants = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Conversation
+        fields = ['participants']
+
+
+class MessageCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for sending a message to a conversation.
+    Sender is automatically set to the current user.
+    """
+    class Meta:
+        model = Message
+        fields = ['conversation', 'content']
