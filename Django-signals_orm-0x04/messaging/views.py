deacf28adeb7ff782b4646e3274e9d205b810ada
Kyei Cundy <nabcoasfeg14756@gmail.com>
2025-06-15 15:53:40 +0100

    for views

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -7,7 +7,8 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from .models import Message
 from django.db.models import Prefetch
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 @login_required
 def delete_user(request):
     user = request.user
@@ -23,3 +24,44 @@ def get_threaded_conversations(user):
         Prefetch('replies', queryset=Message.objects.select_related('sender'))
     )
     return top_level_messages
+
+
+@login_required
+def unread_inbox(request):
+    """
+    View to display unread messages for the logged-in user.
+    """
+    unread_messages = Message.unread.for_user(request.user)
+    return render(request, 'inbox/unread_messages.html', {
+        'unread_messages': unread_messages
+    })
+
+
+@login_required
+def message_detail(request, message_id):
+    """
+    View to show a single message. Marks it as read if unread.
+    """
+    message = get_object_or_404(Message, id=message_id, receiver=request.user)
+
+    # Mark as read if it hasn't been read yet
+    if not message.read:
+        message.read = True
+        message.save(update_fields=['read'])
+
+    return render(request, 'inbox/message_detail.html', {
+        'message': message
+    })
+
+
+@login_required
+def all_messages(request):
+    """
+    Optional: View to display all received messages.
+    """
+    messages = Message.objects.filter(receiver=request.user).select_related('sender').only(
+        'id', 'sender', 'content', 'timestamp', 'read'
+    )
+    return render(request, 'inbox/all_messages.html', {
+        'messages': messages
+    })
