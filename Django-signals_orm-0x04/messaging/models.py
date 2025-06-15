67aae22b0a7293ff26cc4d79325cd512f22b4221
Kyei Cundy <nabcoasfeg14756@gmail.com>
2025-06-15 15:40:30 +0100

    models

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -9,6 +9,26 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    read =models.BooleanField(default=False)
+    parent_message = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver} at {self.content[:30]}"
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'From {self.sender} to {self.receiver} at {self.timestamp}'
