4f988085d51eaaf5382120a0dea00f28a0c7f3e5
Kyei Cundy <nabcoasfeg14756@gmail.com>
2025-06-15 16:55:01 +0100

    updated messaging_app

---
+++ Django-signals_orm-0x04/messaging_app/messaging_app/chats/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    def __str__(self):
+        return self.username
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Conversation {self.id} between {[user.username for user in self.participants.all()]}"
+# Create your models here.
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey('Conversation', related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='messages_sent', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+    def __str__(self):
+        return f"From {self.sender.username} at {self.timestamp}"
